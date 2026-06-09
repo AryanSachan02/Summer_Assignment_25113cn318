46a49f4797a23c73b8a7ce6e4f47d132471fd9d4
AryanSachan02 <aryansachan841@gmail.com>
2026-06-09 13:19:25 +0530

    Day-2 re-Uploaded

---
+++ Day-2/Reverse_no.cpp
@@ -0,0 +1,17 @@
+//Write a program to Reverse a numbe
+#include <iostream>
+using namespace std;
+int main() {
+    int num, reversedNum = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while (num > 0) {
+        int digit = num % 10; 
+        reversedNum = reversedNum * 10 + digit; 
+        num /= 10; 
+    }
+
+    cout << "Reversed number: " << reversedNum << endl;
+    return 0;
+}
