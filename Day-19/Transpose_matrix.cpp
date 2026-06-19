8fa93e534f4ecf0de548029a61d6d5e36dbbf8c9
AryanSachan02 <aryansachan841@gmail.com>
2026-06-19 14:46:16 +0530

    Day-19 Uploaded

---
+++ Day-19/Transpose_matrix.cpp
@@ -0,0 +1,30 @@
+//Write a program to transpose matrix.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter number of rows and columns: ";
+    cin >> rows >> cols;
+
+    int A[10][10];
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    cout << "Transpose of the matrix:\n";
+    for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
