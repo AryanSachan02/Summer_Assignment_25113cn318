ae3534f61d7dabdf036fdad946f8594461e80ef8
AryanSachan02 <aryansachan841@gmail.com>
2026-06-26 22:18:20 +0530

    Day-26 Uploaded

---
+++ Day-26/Guessing_game.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    int secretNumber = rand() % 100 + 1; 
+    int guess, attempts = 0;
+
+    cout << "===== Number Guessing Game =====" << endl;
+    cout << "I have selected a number between 1 and 100." << endl;
+    cout << "Try to guess it!" << endl;
+
+    do {
+        cout << "\nEnter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess > secretNumber) {
+            cout << "Too high! Try again." << endl;
+        } 
+        else if (guess < secretNumber) {
+            cout << "Too low! Try again." << endl;
+        } 
+        else {
+            cout << "\nCongratulations! You guessed the correct number." << endl;
+            cout << "Number: " << secretNumber << endl;
+            cout << "Attempts: " << attempts << endl;
+        }
+
+    } while (guess != secretNumber);
+
+    return 0;
+}
